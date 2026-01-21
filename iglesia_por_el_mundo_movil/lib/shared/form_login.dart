6193f8f47e617b82b0e4cf31471f0be9be34a37b
Germán <german.pastor2006@gmail.com>
2026-01-21 09:00:32 +0100

    subida

--- iglesia_por_el_mundo_movil/lib/shared/form_login.dart
+++ iglesia_por_el_mundo_movil/lib/shared/form_login.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 
 class FormLogin extends StatelessWidget {
-  const FormLogin({super.key,required this.texto,required this.color});
+  const FormLogin({super.key,required this.texto,required this.color, this.obscureText = false});
 
   final String texto;
   final Color color;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 310,
-      height: 50,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10)
-      ),
-      child: Padding(
-        padding: const EdgeInsetsGeometry.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(texto, style: TextStyle(fontSize: 20),)
-              ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 10),
+        Container(
+          width: 310,
+          height: 50,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              obscureText: obscureText,
+              decoration: InputDecoration(
+                labelText: texto,
+                border: InputBorder.none,
+                labelStyle: TextStyle(fontSize: 16),
+                prefixIcon: Icon(Icons.account_circle),
+              ),
+              style: TextStyle(fontSize: 18),
             ),
-          ],
+          ),
         ),
-        ),
+      ],
     );
   }
 }
--- iglesia_por_el_mundo_movil/lib/ui/login.dart
+++ iglesia_por_el_mundo_movil/lib/ui/login.dart
@@ -10,29 +10,53 @@ class Login extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(50),
         color: const Color.fromARGB(104, 187, 187, 187),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FormLogin(
-                  texto: 'Usuario',
-                  color: const Color.fromARGB(104, 179, 178, 178),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 100),
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  fit: BoxFit.contain,
                 ),
-              ],
-            ),
-            SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FormLogin(
-                  texto: 'Contraseña',
-                  color: const Color.fromARGB(104, 179, 178, 178),
+              ),
+              SizedBox(height: 20),
+              const Text('Iglesia por el Mundo', style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal),),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FormLogin(
+                    texto: 'Usuario',
+                    color: const Color.fromARGB(104, 179, 178, 178),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FormLogin(
+                    texto: 'Contraseña',
+                    color: const Color.fromARGB(104, 179, 178, 178),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+              SizedBox(height: 30,),
+              SizedBox(
+                width: 350,
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: const Text('Iniciar Sesión'),
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
